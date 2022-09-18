791823a9037a1a3fe6fac3458cab858688162b5b
Alex <38843286+BeAllAround@users.noreply.github.com>
2022-09-18 15:15:54 +0200

    First refactor

--- script.js
+++ script.js
@@ -1,40 +1,75 @@
-let text = "Let's see how fast you can type, shall we?";
+function typer(text) {
+        let speed = document.querySelector("#speed");
+	let accuracy = document.querySelector("#accuracy");
+	let canvas = document.querySelector("#canvas");
 
-let checker = 0;
-let output = "";
-const list = new Array();let c=0;
-let e;
-let start, _final;
-let c1 = 0;
-let acc = 100;
+	text = text.split(' ').filter(v=>v!=='').join(' ') // white-space hack
 
-const canvas = document.querySelector("#canvas");
-const speed = document.querySelector("#speed");
-const accuracy = document.querySelector("#accuracy");
+	let checker = true;
+	let output = "";
+	let list = []
+	let c = 0; // character counter
+	let start, _final;
+	let c1 = 0;
+	let acc = 100;
+	let words
 
-const reFRESH = (_text, list, elem)=>{
-	for(let i = 0; i<_text.length; i++){
-		e = document.createElement('span');
-		e.textContent = _text[i];
+
+        if(!c1){start = (new Date()).getTime(); c1 = 1;};
+        reFRESH(text, list, canvas);
+        document.addEventListener('keypress', logKey);
+
+	const interlude = setInterval(()=>{
+        	_final = ((new Date()).getTime() - start);
+        	speed.textContent = (wps(output.length, _final/1000)).toString();
+        	accuracy.textContent = FormatAcc(acc);
+	}, 200);
+
+    	function clear(){
+        	clearInterval(interlude);
+        	// document.dispatchEvent; // in the works;
+        	canvas.textContent = "DONE!";
+        	speed.textContent = (wps(output.length, _final/1000)).toString();
+        	accuracy.textContent = FormatAcc(acc);
+    	}
+
+function simpleSpan(content) {
+	let elem = document.createElement('span')
+	elem.textContent = content
+	return elem
+}
+
+function reFRESH(_text, list, elem){
+	console.log('_text: ', words = _text.split(' ').filter(v=>v!=='').map((v,i)=>[i,v]))
+	for(let i = 0; i<words.length; i++){
+		let e;
+		e = document.createElement('div');
+		e.id = 'word' + words[i][0]
+		Object.assign(e.style, {display: 'inline',})
+		for(let word of words[i][1]) {
+			e.appendChild(simpleSpan(word))
+		}
+		e.appendChild(simpleSpan(' '))
 		list.push(e);
 	}
 
-	elem.innerHTML = "&nbsp;"  // set it up!
+	console.log('elem: ', elem)
+	console.log('list: ', list)
 	for(let i = 0; i<list.length; i++){
-		elem.append(list[i]);
+		elem.appendChild(list[i]);
 	}
-	setUpAccuracy();
+	setUpAccuracy(accuracy);
 }
 
-const FormatAcc = (s)=>{
+function FormatAcc(s){
 	return "ACCURACY: " + s.toString() + "%";
 }
 
-const setUpAccuracy=()=>{
-	accuracy.textContent = FormatAcc(acc);
+function setUpAccuracy(elem){
+	elem.textContent = FormatAcc(acc);
 };
 
-const innerHtmlGenerator = (list)=>{ // superfluous
+function innerHtmlGenerator(list){ // superfluous
 	let _string = "";
 	for(let i = 0; i<list.length; i++){
 		_string+=list[i].innerHTML;
@@ -44,46 +79,41 @@ const innerHtmlGenerator = (list)=>{ // superfluous
 
 
 
-const wps = (length, secondsPassed)=>{ // f(value);
+function wps(length, secondsPassed){ // f(value);
 	return (length*60)/(5*secondsPassed);
 }
 
+let C = 0
+function logKey(e){
+if(checker) {
+	let key = e.key, b, word, ch
+	word = list[C] // current word
+	console.log('current word: ', word)
 
-const interlude = setInterval(()=>{
-	_final = ((new Date()).getTime() - start);
-	speed.textContent = (wps(output.length, _final/1000)).toString();
-	accuracy.textContent = FormatAcc(acc);
-}, 200);
+	Object.assign(word.style, {'background-color': 'green',})
 
-const clear=()=>{
-	clearInterval(interlude);
-	// document.dispatchEvent; // in the works;
-	canvas.textContent = "DONE!";
-	speed.textContent = (wps(output.length, _final/1000)).toString();
-	accuracy.textContent = FormatAcc(acc);
-};
-
-const logKey=e=>{
-	//console.log(list[c]);
-	if(!checker){
-		if(e.key==list[c].textContent){
-			output += String(e.key);
-			list[c].style.background = 'blue';
-			c++; // next;
-			if(c==list.length){
-				checker = 1;
-				clear();
-			}
-		}else{ // else  diminish the accuracy;
-			acc -= 0.3;
+	if(word.children[c].textContent == key) {
+		output += String(e.key); // for the calculation
+		ch = word.children[c++]
+		ch.style.background = 'blue'
+		if(word.children.length == c) {
+			c = 0 // reset couter for the next word
+			C++ // next word
 		}
-	}; 
-	// do nothing;
-};
+	}else{ // else  diminish the accuracy;
+		acc -= 0.3;
+	}
 
-(()=>{
-	if(!c1){start = (new Date()).getTime(); c1 = 1;};
-	reFRESH(text, list, canvas);
-	document.addEventListener('keypress', logKey);
-})();
+	if(text == output) {
+                clear()
+                checker = false
+                return
+        }
 
+    }
+}
+
+}
+
+let text = "Let's see how fast you can type, shall we?";
+new typer(text)
